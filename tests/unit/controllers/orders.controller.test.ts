884835a713d6f8f0afba59ecb5e7b427f63450d5
Mariana Pereira <marianapereira.s@live.com>
2024-01-17 22:00:05 -0300

    Requisito 2: cobertura de testes em 50%

--- tests/unit/controllers/orders.controller.test.ts
+++ tests/unit/controllers/orders.controller.test.ts
@@ -1,18 +1,70 @@
+// orders.controller.test.ts
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import { Request, Response } from 'express';
+import ordersService from '../../../src/services/orders.service';
+import ordersController from '../../../src/controllers/orders.controller';
+import { ServiceResponse } from '../../../src/types/ServiceResponse';
 
 chai.use(sinonChai);
 
 describe('OrdersController', function () {
-  const req = {} as Request;
-  const res = {} as Response;
+  const req: any = {};
+  const res: any = {
+    status: sinon.stub().returnsThis(),
+    json: sinon.stub(),
+  };
 
   beforeEach(function () {
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns(res);
     sinon.restore();
   });
 
+  describe('list', function () {
+    it('should return data with 200 status if service response is successful', async function () {
+      const serviceResponse: ServiceResponse<Array<{
+        id: number;
+        userId: number;
+        productIds: number[];
+      }>> = {
+        status: 'SUCCESSFUL',
+        data: [
+          {
+            id: 1,
+            userId: 123,
+            productIds: [ 101, 102, 103 ],
+          },
+          {
+            id: 2,
+            userId: 456,
+            productIds: [ 104, 105 ],
+          },
+        ],
+      };
+
+      sinon.stub(ordersService, 'list').resolves(serviceResponse);
+
+      await ordersController.list(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(serviceResponse.data);
+    });
+
+    it('should return error response if service response is not successful', async function () {
+      const serviceResponse: ServiceResponse<Array<{
+        id: number;
+        userId: number;
+        productIds: number[];
+      }>> = {
+        status: 'NOT_FOUND',
+        data: { message: 'Error message' },
+      };
+
+      sinon.stub(ordersService, 'list').resolves(serviceResponse);
+
+      await ordersController.list(req, res);
+
+      expect(res.status).to.have.been.calledWith();
+      expect(res.json).to.have.been.calledWith(serviceResponse.data);
+    });
+  });
 });
--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,6 +2,8 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import productsController from '../../../src/controllers/products.controller';
+import productsService from '../../../src/services/products.service';
 
 chai.use(sinonChai);
 
@@ -15,4 +17,17 @@ describe('ProductsController', function () {
     sinon.restore();
   });
 
+  describe('create', function () {
+    it('should create a product and return it with a 201 status', async function () {
+      req.body = { name: 'Product 1', price: 19.99, orderId: 1 };
+
+      const productData = { id: 1, name: 'Product 1', price: 19.99, orderId: 1 };
+      sinon.stub(productsService, 'create').resolves(productData);
+
+      await productsController.create(req, res);
+
+      expect(res.status).to.be.calledWith(201);
+      expect(res.json).to.be.calledWith(productData);
+    });
+  });
 });
