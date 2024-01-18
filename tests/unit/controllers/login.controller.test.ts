5840be10b46e5f2085380e1ce29166248032d058
Mariana Pereira <marianapereira.s@live.com>
2024-01-17 21:51:23 -0300

    Requisito 5 funcional + testes do requisito 1

--- tests/unit/controllers/login.controller.test.ts
+++ tests/unit/controllers/login.controller.test.ts
@@ -2,17 +2,38 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import loginService from '../../../src/services/login.service';
+import loginController from '../../../src/controllers/login.controller';
+import { Token } from '../../../src/types/Token';
+import { ServiceResponse } from '../../../src/types/ServiceResponse';
 
 chai.use(sinonChai);
 
 describe('LoginController', function () {
-  const req = {} as Request;
-  const res = {} as Response;
-
   beforeEach(function () {
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns(res);
     sinon.restore();
   });
 
+  describe('login', function () {
+    it('should return user data with 200 status if login is successful', async function () {
+      const req: any = { body: { username: 'user1', password: 'password123' } };
+      const res: any = { status: sinon.stub(), json: sinon.stub() };
+
+      const serviceResponse: ServiceResponse<Token> = {
+        status: 'SUCCESSFUL',
+        data: { token: 'some_token' },
+      };
+
+      try {
+        sinon.stub(loginService, 'verifyLogin').resolves(serviceResponse);
+
+        await loginController.login(req, res);
+
+        expect(res.status.calledWith(200)).to.be.true;
+        expect(res.json.calledWith(serviceResponse.data)).to.be.true;
+      } catch (error) {
+        console.error('Error during test:', error);
+      }
+    });
+  });
 });
